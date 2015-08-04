118791ff3d5b5699cd6f7bf15ba988388920ab8e
irene lin <irenelin@techbangtekiMac-mini.local>
2015-08-04 12:06:14 +0800

    $ rails g without assets

--- config/application.rb
+++ config/application.rb
@@ -16,6 +16,11 @@ Bundler.require(*Rails.groups)
 
 module Tb101
   class Application < Rails::Application
+
+    config.generators do |g|
+      g.assets false
+    end
+
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
