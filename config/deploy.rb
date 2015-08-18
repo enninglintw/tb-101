9a216cc29cff907e81a0459acbb2aba8955395ff
irene lin <irenelin@techbangtekiMac-mini.local>
2015-08-18 13:55:06 +0800

    fix: config/deploy.rb

--- config/deploy.rb
+++ config/deploy.rb
@@ -42,7 +42,14 @@ set :linked_files, %w(config/database.yml config/secrets.yml)
 # Default value for linked_dirs is []
 # set :linked_dirs, fetch(:linked_dirs, []).push('log', 'tmp/pids', 'tmp/cache', 'tmp/sockets', 'vendor/bundle', 'public/system')
 # linked_dirs: after git clone is finished, the folders would be copied from shared folder
-set :linked_dirs, fetch(:linked_dirs, []).push("bin", "log", "tmp/pids", "tmp/cache", "tmp/sockets", "vendor/bundle", "public/system")
+set :linked_dirs, %w{
+  log
+  tmp/pids
+  tmp/cache
+  tmp/sockets
+  vendor/bundle
+  public/system
+}
 
 # Default value for default_env is {}
 # set :default_env, { path: "/opt/ruby/bin:$PATH" }
